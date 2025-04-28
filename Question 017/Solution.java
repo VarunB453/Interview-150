95d4523e48a3734001680dabe3d4fb5b6c71ed1a
Varun Bankar <116241000+VarunB453@users.noreply.github.com>
2025-04-29 01:46:15 +0530

    Update Solution.java

--- Question 017/Solution.java
+++ Question 017/Solution.java
@@ -1,22 +1,26 @@
 class Solution {
   public int romanToInt(String s) {
-    int ans = 0;
-    int[] roman = new int[128];
-
-    roman['I'] = 1;
-    roman['V'] = 5;
-    roman['X'] = 10;
-    roman['L'] = 50;
-    roman['C'] = 100;
-    roman['D'] = 500;
-    roman['M'] = 1000;
-
-    for (int i = 0; i + 1 < s.length(); ++i)
-      if (roman[s.charAt(i)] < roman[s.charAt(i + 1)])
-        ans -= roman[s.charAt(i)];
-      else
-        ans += roman[s.charAt(i)];
-
-    return ans + roman[s.charAt(s.length() - 1)];
+    Map<Character, Integer> roman = Map.of(
+      'I', 1,
+      'V', 5,
+      'X', 10,
+      'L', 50,
+      'C', 100,
+      'D', 500,
+      'M', 1000
+    );
+    
+    int total = 0;
+    
+    for (int i = 0; i < s.length(); i++) {
+      int value = roman.get(s.charAt(i));
+      if (i + 1 < s.length() && value < roman.get(s.charAt(i + 1))) {
+        total -= value;
+      } else {
+        total += value;
+      }
+    }
+    
+    return total;
   }
 }
