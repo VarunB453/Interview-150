d98ba5f62c0778e1e897fee4ef3f29f4a0a568b4
Varun Bankar <116241000+VarunB453@users.noreply.github.com>
2025-05-19 00:00:04 +0530

    Update Solution.java

--- Question 040/Solution.java
+++ Question 040/Solution.java
@@ -1,1 +1,11 @@
-public
+public class Solution {
+  public boolean isIsomorphic(String s, String t) {
+    Map<Character, Integer> A = new HashMap<>();
+    Map<Character, Integer> B = new HashMap<>();
+
+    for (Integer i = 0; i < s.length(); ++i)
+      if (A.put(s.charAt(i), i) != B.put(t.charAt(i), i))
+        return false;
+    return true;
+  }
+}
